b80bc91386edefeab367e274d72ec51a773b3ca3
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-04-13 00:44:29 +0200

    - added Task entity - added TaskCreatedOn value object

---
+++ ToDoApp/Domain/TasksDomain/Entities/Task.cs
@@ -0,0 +1,23 @@
+﻿using Domain.TasksDomain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.TasksDomain.Entities
+{
+	public class Task(int id, string title, string description)
+	{
+		public int Id { get; set; } = id;
+		public string Title { get; set; } = title;
+		public string Description { get; set; } = description;
+		public bool IsCompleted { get; set;}
+		public TaskCreatedOn CreatedOn { get; private set; } = new TaskCreatedOn(DateOnly.FromDateTime(DateTime.UtcNow));
+
+		public void CompleteTask()
+		{
+			IsCompleted = true;
+		}
+	}
+}
---
+++ ToDoApp/Domain/TasksDomain/ValueObjects/TaskCreatedOn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.TasksDomain.ValueObjects
+{
+	public class TaskCreatedOn
+	{
+		public DateOnly CreationDate {  get; private set; }
+
+		public TaskCreatedOn(DateOnly createdOn)
+		{	
+			CreationDate = createdOn;
+		}
+	}
+}
