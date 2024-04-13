7c20f27c9c158a124eb91621096dbab6be60f574
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-04-13 12:58:06 +0200

    - added Error and Result abstractions to Domain so the Application service returns custom meaningful errors - added TodoErrors class where custom Todo errors are defined

---
+++ ToDoApp/Domain/ToDo/Errors/TodoErrors.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.ToDo.Errors
+{
+	public static class TodoErrors
+	{
+		public static readonly Error NotFound = new("Todo.NotFound", "Couldn't find the requested ToDo");
+
+
+	}
+}
