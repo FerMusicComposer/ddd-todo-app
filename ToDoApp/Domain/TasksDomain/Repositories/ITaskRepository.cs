4f0148c18c761cc16da56da4bf63f766d3c64cf3
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2024-04-13 02:03:40 +0200

    - removed unused usings - added Tasks repository interface

---
+++ ToDoApp/Domain/TasksDomain/Repositories/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.TasksDomain.Repositories
+{
+	public interface ITaskRepository
+	{
+		Task<Task> GetTaskAsync(int id);
+		Task<IEnumerable<Task>> GetAllTasksAsync();
+		Task AddTaskAsync(Task task);
+		Task UpdateTaskAsync(int id);
+		Task DeleteTaskAsync(int id);
+	}
+}
